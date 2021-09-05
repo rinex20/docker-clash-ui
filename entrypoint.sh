a669d3ca19a84f79c41f8b04ba55195bd463d269
rinex20 <rinex20@me.com>
2021-09-05 14:21:52 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 
 ipset create localnetwork hash:net
 ipset add localnetwork 127.0.0.0/8
-ipset add localnetwork 0.0.0.0/8
 ipset add localnetwork 10.0.0.0/8
 ipset add localnetwork 169.254.0.0/16
 ipset add localnetwork $LOCAL_IP
@@ -28,24 +27,27 @@ if [ "$MODE" == "tproxy" ]; then
 
 elif [ "$MODE" == "tun" ]; then
   # Based on https://github.com/Kr328/kr328-clash-setup-scripts/blob/master/setup-clash-tun.sh
-  ip route replace default dev utun table 129
-  ip rule add fwmark 129 lookup 129
 
-  iptables -t mangle -N CLASH
-  iptables -t mangle -F CLASH
-  iptables -t mangle -A CLASH -p tcp --dport 53 -j MARK --set-mark 129
-  iptables -t mangle -A CLASH -p udp --dport 53 -j MARK --set-mark 129
-  iptables -t mangle -A CLASH -m addrtype --dst-type BROADCAST -j RETURN
-  iptables -t mangle -A CLASH -m set --match-set localnetwork dst -j RETURN
-  iptables -t mangle -A CLASH -d 198.18.0.0/16 -j MARK --set-mark 129
-  iptables -t mangle -A CLASH -j MARK --set-mark 129
 
-  iptables -t mangle -I OUTPUT -j CLASH
-  iptables -t mangle -I PREROUTING -m set ! --match-set localnetwork dst -j MARK --set-mark 129
-  
-  sysctl -w net/ipv4/ip_forward=1
-  sysctl -w net.ipv4.conf.utun.rp_filter=0
-  sysctl -w net.ipv4.conf.all.rp_filter=0
+ip route replace default dev utun table 0x162
+
+ip rule add fwmark 0x162 lookup 0x162
+
+iptables -t mangle -N CLASH
+iptables -t mangle -F CLASH
+iptables -t mangle -A CLASH -p tcp --dport 53 -j MARK --set-mark 0x162
+iptables -t mangle -A CLASH -p udp --dport 53 -j MARK --set-mark 0x162
+iptables -t mangle -A CLASH -m addrtype --dst-type BROADCAST -j RETURN
+iptables -t mangle -A CLASH -m set --match-set localnetwork dst -j RETURN
+iptables -t mangle -A CLASH -d 198.18.0.0/16 -j MARK --set-mark 0x162
+iptables -t mangle -A CLASH -j MARK --set-mark 0x162
+
+iptables -t mangle -I OUTPUT -j CLASH
+iptables -t mangle -I PREROUTING -m set ! --match-set localnetwork dst -j MARK --set-mark 0x162
+
+
+#sysctl -w net/ipv4/ip_forward=1
+#sysctl -w net.ipv4.conf.utun0.rp_filter=0
   
 elif [ "$MODE" == "redir" ]; then
   # Bypass private IP address ranges
