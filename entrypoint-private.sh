8b18f1e89392097de494df35bbb8ee33246fb206
rinex20 <rinex20@me.com>
2021-09-05 11:51:26 +0800

    Update entrypoint-private.sh

--- entrypoint-private.sh
+++ entrypoint-private.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+ipset create localnetwork hash:net
+  ipset add localnetwork 127.0.0.0/8
+  ipset add localnetwork 10.0.0.0/8
+  ipset add localnetwork 169.254.0.0/16
+  ipset add localnetwork $LOCAL_IP
+  ipset add localnetwork 224.0.0.0/4
+  ipset add localnetwork 240.0.0.0/4
+  ipset add localnetwork 172.16.0.0/12
+
 iptables -t mangle -N CLASH
  iptables -t mangle -F CLASH
  iptables -t mangle -A CLASH -d 0.0.0.0/8 -j RETURN
