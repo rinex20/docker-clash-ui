2bc76fc06f0b704eee500fe5d750da349a8d7294
rinex20 <rinex20@me.com>
2021-09-06 13:08:08 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-chmod a+x /root/clash/entrypoint.sh
 
+if [ -f "/root/clash/entrypoint.sh" ]; then
+chmod a+x /root/clash/entrypoint.sh
 /root/clash/entrypoint.sh
+else 
+chmod a+x ./entrypoint.sh
+./entrypoint.sh
+fi
