4d78d8a236acc55867cb51b18613f0cdd7ded041
rinex20 <rinex20@me.com>
2022-03-01 11:48:41 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+set -e
+exec /clash
+
 ipset create localnetwork hash:net
 ipset add localnetwork 127.0.0.0/8
 ipset add localnetwork 10.0.0.0/8
@@ -16,20 +19,19 @@ ip rule add fwmark 0x129 lookup 0x129
 
 iptables -t mangle -N CLASH
 iptables -t mangle -F CLASH
-iptables -t mangle -A CLASH -p tcp --dport 53 -j MARK --set-mark 0x129
-iptables -t mangle -A CLASH -p udp --dport 53 -j MARK --set-mark 0x129
+iptables -t mangle -A CLASH -p tcp --dport 53 -j RETURN #MARK --set-mark 0x129
+iptables -t mangle -A CLASH -p udp --dport 53 -j RETURN #MARK --set-mark 0x129
 iptables -t mangle -A CLASH -m addrtype --dst-type BROADCAST -j RETURN
 iptables -t mangle -A CLASH -m set --match-set localnetwork dst -j RETURN
 iptables -t mangle -A CLASH -d 198.18.0.0/16 -j MARK --set-mark 0x129
 iptables -t mangle -A CLASH -j MARK --set-mark 0x129
 
 iptables -t mangle -I OUTPUT -j CLASH
-iptables -t mangle -I PREROUTING -m set ! --match-set localnetwork dst -j MARK --set-mark 0x129
+#iptables -t mangle -I PREROUTING -m set ! --match-set localnetwork dst -j MARK --set-mark 0x129
+iptables -t mangle -I PREROUTING -j CLASH
 
 sysctl -w net/ipv4/ip_forward=1
-sysctl -w net.ipv4.conf.utun0.rp_filter=0
-
-iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE
+sysctl -w net.ipv4.conf.utun.rp_filter=0
 
 exec "$@"
 
