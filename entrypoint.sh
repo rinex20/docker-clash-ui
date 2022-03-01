7d2bb0dc93db513b904814d9ce454cbc31322dd8
rinex20 <rinex20@me.com>
2022-03-01 12:18:49 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,37 +1,49 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+
+curl -fsSL http://192.168.1.1/clash/90.6/config-tun.yaml -o /root/.config/clash/config.yaml
+
 exec /clash
 
-ipset create localnetwork hash:net
-ipset add localnetwork 127.0.0.0/8
-ipset add localnetwork 10.0.0.0/8
-ipset add localnetwork 169.254.0.0/16
-ipset add localnetwork 192.168.0.0/16
-ipset add localnetwork 224.0.0.0/4
-ipset add localnetwork 240.0.0.0/4
-ipset add localnetwork 172.16.0.0/12
+while true; do
+    ip link show utun
+    [ $? -eq 0 ] && break
+    echo -e "\033[32m正在启动clash，请等待...\033[0m"
+    sleep 3
+done
 
-# Based on https://github.com/Kr328/kr328-clash-setup-scripts/blob/master/setup-clash-tun.sh
+#    ip tuntap add utun mode tun user nobody
+#    ip link set utun up
+#    ip addr add "198.18.0.1/16" dev utun
 
-ip route replace default dev utun table 0x129
-ip rule add fwmark 0x129 lookup 0x129
+ip route replace default dev utun table 129
 
-iptables -t mangle -N CLASH
-iptables -t mangle -F CLASH
-iptables -t mangle -A CLASH -p tcp --dport 53 -j RETURN #MARK --set-mark 0x129
-iptables -t mangle -A CLASH -p udp --dport 53 -j RETURN #MARK --set-mark 0x129
-iptables -t mangle -A CLASH -m addrtype --dst-type BROADCAST -j RETURN
-iptables -t mangle -A CLASH -m set --match-set localnetwork dst -j RETURN
-iptables -t mangle -A CLASH -d 198.18.0.0/16 -j MARK --set-mark 0x129
-iptables -t mangle -A CLASH -j MARK --set-mark 0x129
+ip rule del fwmark 129 lookup 129 > /dev/null 2> /dev/null
+ip rule add fwmark 129 lookup 129
 
-iptables -t mangle -I OUTPUT -j CLASH
-#iptables -t mangle -I PREROUTING -m set ! --match-set localnetwork dst -j MARK --set-mark 0x129
-iptables -t mangle -I PREROUTING -j CLASH
+nft -f - << EOF
+define LOCAL_SUBNET = { 10.0.0.0/8, 127.0.0.0/8, 169.254.0.0/16, 172.16.0.0/12, 192.168.0.0/16, 224.0.0.0/4, 240.0.0.0/4 }
+table clash
+flush table clash
+table clash {
+    chain forward {
+        type filter hook prerouting priority 0; policy accept;           
+        ip protocol != { tcp, udp } accept        
+        iif utun accept
+        ip daddr \$LOCAL_SUBNET accept           
+        ip protocol { tcp, udp } mark set 129
+    }
+    chain forward-dns-redirect {
+        type nat hook prerouting priority 0; policy accept;           
+        ip protocol != { tcp, udp } accept        
+        ip daddr \$LOCAL_SUBNET tcp dport 53 dnat :5352
+        ip daddr \$LOCAL_SUBNET udp dport 53 dnat :5352
+    }
+}
+EOF
 
-sysctl -w net/ipv4/ip_forward=1
-sysctl -w net.ipv4.conf.utun.rp_filter=0
+ip addr
 
-exec "$@"
-
+echo -e "\033[32mclash服务已启动！\033[0m"
+echo -e "\033[32m请使用 http://<容器ip>:9090/ui 管理内置规则\033[0m"
